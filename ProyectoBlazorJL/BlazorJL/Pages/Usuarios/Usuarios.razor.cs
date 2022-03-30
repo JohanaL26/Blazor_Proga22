1f22d5a092e0c91d3bba7c66cac8cc53c32b56e6
JohanaL26 <johanalara2606@gmail.com>
2022-03-30 15:52:58 -0600

    Update Usuarios.razor.cs

--- ProyectoBlazorJL/BlazorJL/Pages/Usuarios/Usuarios.razor.cs
+++ ProyectoBlazorJL/BlazorJL/Pages/Usuarios/Usuarios.razor.cs
@@ -11,7 +11,7 @@ namespace BlazorJL.Pages.Usuarios;
         private IEnumerable<Usuario> usuariosLista { get; set; }
 
 
-        protected async Task OnInitializedAsync()
+        protected override async Task OnInitializedAsync()
         {
             usuariosLista = await _usuarioServicio.GetLista();
         }
